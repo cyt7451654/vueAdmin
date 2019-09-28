cd5219d5778336493efd999ffb69f96eeb8e95d1
hqy <15868019732@163.com>
2019-09-28 14:56:47 +0800

    基础模板

---
+++ src/components/table/table.vue
@@ -0,0 +1,123 @@
+
+<template>
+  <div class="components">
+    <el-table
+      border
+      :data="dataSource"
+      @selection-change="handleSelectionChange"
+    >
+      <!--选择-->
+      <el-table-column v-if="hasSelection" type="selection" width="55" :align="textAlign" />
+      <!--序号-->
+      <el-table-column v-if="hasIndex" type="index" width="55" :align="textAlign" />
+      <!--数据源-->
+      <el-table-column
+        v-for="column in columns"
+        v-show="column.isShow"
+        :key="column.prop"
+        :header-align="headerAlign"
+        :sortable="column.hasSort"
+        :prop="column.prop"
+        :label="column.label"
+        :align="textAlign"
+        :width="column.width"
+      />
+      <!--操作-->
+      <slot name="slotItem" />
+    </el-table>
+    <div class="pagination">
+      <el-pagination
+        v-if="showPagination==true"
+        :page-size="size"
+        :current-page="page"
+        :total="total"
+        background
+        layout="prev, pager, next, jumper"
+        @current-change="pageChange"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TmTable',
+  props: {
+    // 表头对齐方式
+    headerAlign: {
+      type: String,
+      default: 'left'
+    },
+    // 表内容对齐方式
+    textAlign: {
+      type: String,
+      default: 'left'
+    },
+    // 是否可以选择
+    hasSelection: {
+      type: Boolean,
+      default: function() {
+        return false
+      }
+    },
+    // 是否有序列项
+    hasIndex: {
+      type: Boolean,
+      default: function() {
+        return false
+      }
+    },
+    // 这是相应的字段展示
+    columns: {
+      type: Array,
+      default: function() {
+        return []
+      }
+    },
+    // 这是数据源
+    dataSource: {
+      type: Array,
+      default: function() {
+        return []
+      }
+    },
+    showPagination: {
+      type: Boolean,
+      default: false
+    },
+    // 分页相关数据
+    page: {
+      type: Number,
+      default: 1
+    },
+    size: {
+      type: Number,
+      default: 10
+    },
+    total: {
+      type: Number,
+      default: 0
+    }
+  },
+  methods: {
+    // 将选中的行发送到父组件
+    handleSelectionChange(val) {
+      const selectionArr = []
+      val.forEach(function(el) {
+        selectionArr.push(el)
+      })
+      this.$emit('commitSelection', selectionArr)
+    },
+    pageChange(val) {
+      this.$emit('pageChange', val)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.pagination{
+  margin-top: 30px;
+  text-align: right;
+}
+</style>
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="app-container">
+     <my-table
+       :columns="tableColumns"
+       :data-source="tableData"
+       :text-align="`center`"
+       :header-align="'center'"
+       :show-pagination="true"
+       :page="page"
+       :size="size"
+       :total="total"
+       @pageChange="pageChange"
+     >
+       <div slot="slotItem">
+         <el-table-column fixed="right" label="操作" align="center">
+           <template slot-scope="scope">
+             <el-button type="text" size="small" @click="edit(scope.row.id)">编辑</el-button>
+             <el-button v-if="identity=='admin'" type="text" size="small" @click="del(scope.row.id)">删除</el-button>
+           </template>
+         </el-table-column>
+       </div>
+     </my-table>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import myTable from '../../components/table/table.vue'
+export default {
+  name: 'Dashboard',
+  components:{
+    myTable
+  },
+  data(){
+    return{
+      page:1,
+      total:0,
+      size:10,
+      tableColumns: [
+        {
+          hasSort: false, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'province', // <String>  对应属性名
+          label: '省', // <String>   表头标签
+          textalgin: 'center' // <String>   表格内容对齐方式
+        },
+        {
+          hasSort: false, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'city', // <String>  对应属性名
+          label: '市', // <String>   表头标签
+          textalgin: 'center' // <String>   表格内容对齐方式
+        },
+        {
+          hasSort: false, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'district', // <String>  对应属性名
+          label: '区', // <String>   表头标签
+          textalgin: 'center' // <String>   表格内容对齐方式
+        },
+        {
+          hasSort: false, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'campus', // <String>  对应属性名
+          label: '校区', // <String>   表头标签
+          textalgin: 'center' // <String>   表格内容对齐方式
+        },
+        {
+          hasSort: false, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'principal', // <String>  对应属性名
+          label: '校长', // <String>   表头标签
+          textalgin: 'center' // <String>   表格内容对齐方式
+        },
+        {
+          hasSort: false, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'mobile', // <String>  对应属性名
+          label: '客服电话', // <String>   表头标签
+          textalgin: 'center' // <String>   表格内容对齐方式
+        },
+        {
+          hasSort: false, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'username', // <String>  对应属性名
+          label: '账号', // <String>   表头标签
+          textalgin: 'center' // <String>   表格内容对齐方式
+        },
+        {
+          hasSort: false, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'password', // <String>  对应属性名
+          label: '密码', // <String>   表头标签
+          textalgin: 'center' // <String>   表格内容对齐方式
+        },
+        {
+          hasSort: false, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'principalCode', // <String>  对应属性名
+          label: '校长码', // <String>   表头标签
+          textalgin: 'center' // <String>   表格内容对齐方式
+        },
+        {
+          hasSort: false, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'teacherCode', // <String>  对应属性名
+          label: '教师码', // <String>   表头标签
+          textalgin: 'center' // <String>   表格内容对齐方式
+        },
+        {
+          hasSort: false, // <Boolean> 是否排序
+          isShow: true, // <Boolean> 是否展示
+          prop: 'studentCode', // <String>  对应属性名
+          label: '学生码', // <String>   表头标签
+          textalgin: 'center' // <String>   表格内容对齐方式
+        }
+      ],
+      tableData: [],
+    }
+  },
+  methods:{
+    pageChange(val){
+      console.log(val)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
